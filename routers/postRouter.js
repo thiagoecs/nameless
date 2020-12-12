5c9937a4229afe7390e2cb47e1ec9d7477b7869f
mingg1 <comg2843@gmail.com>
2020-12-13 00:46:41 +0200

    editting routers

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -1,5 +1,5 @@
 "use strict";
-// for routes using posts route
+// for routes using posts route (/posts)
 const express = require("express");
 const passport = require("passport");
 const postRouter = express.Router();
@@ -7,27 +7,26 @@ const { postHome, postDetail, getUpload, postUpload, getEditPost, deletePost, po
 const { verifyToken, uploadFiles, loggedUser } = require("../middlewares");
 const routes = require("../routes");
 
-// getting all posts' data
+// getting all posts' data (.../app/posts/)
 postRouter.get(routes.home, postHome);
 
 // upload a post
 postRouter
   .route(routes.upload)
-  .get( verifyToken, getUpload)
+  .get(verifyToken, getUpload)
   .post(passport.authenticate("jwt", { session: false }), loggedUser, uploadFiles, postUpload);
 
 // post detail page
 postRouter
-  .route("/:id")
+  .route(routes.postDetail())
   .get(addViews, postDetail)
   .put(passport.authenticate("jwt", { session: false }), postEditPost)
-  .delete(deletePost);
+  .delete(passport.authenticate("jwt", { session: false }), deletePost);
 
 // edit a post
-postRouter.get("/:id/edit", verifyToken, getEditPost);
 postRouter.post("/:id/edit", verifyToken, loggedUser, uploadFiles, postEditPost);
 
-postRouter.get("/:id/delete", verifyToken, deletePost);
-postRouter.post("/:id/comment", verifyToken, postAddComment);
+postRouter.get("/:id/delete", passport.authenticate("jwt", { session: false }), verifyToken, deletePost);
+postRouter.post("/:id/comment", passport.authenticate("jwt", { session: false }), verifyToken, postAddComment);
 
 module.exports = postRouter;
