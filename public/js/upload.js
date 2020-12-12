5a20e55f92277c4443405b212166cbbb062216e8
mingg1 <comg2843@gmail.com>
2020-12-12 09:02:56 +0200

    fixing upload page

--- public/js/upload.js
+++ public/js/upload.js
@@ -5,22 +5,22 @@ const form = document.querySelector("#upload");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const myProfile = await getMyProfile();
-  const myId = myProfile.id
+  const myId = myProfile.id;
   try {
     const token = document.cookie.split("userToken=")[1];
     const formData = new FormData(form);
-console.log(token)
+    console.log(formData);
     // receiving json data from backend when submit login request
     const res = await fetch("/app/posts/upload", {
       method: "POST",
       headers: { Authorization: "Bearer" + token },
-      body: formData+myId,
+      body: formData,
     });
     const data = await res.json();
-    console.log(data)
+    console.log(data);
     if (data) {
-      console.log(data)
-     // location.assign(URL_BASE + "/");
+      console.log(data);
+      // location.assign(URL_BASE + "/");
     }
   } catch (err) {
     console.log(err);
