ed0dd13b901c8e2ac6c0aac9ff0a62e3a21ddefc
mingg1 <comg2843@gmail.com>
2020-12-13 19:14:20 +0200

    fixing error in post controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -105,6 +105,7 @@ const postUpload = async (req, res) => {
     await postModel.insertFiles(newPost, path);
     res.status(201).json({ message: "file uploaded" });
   } catch (err) {
+    console.log(err)
     res.status(400).json({err});
   }
 };
